da9daaf360076e91b40549c4b8431a5b2c0264ab
Diemond_Player <diemondplay@gmail.com>
2024-05-12 11:41:13 +0500

    Initial commit

---
+++ src/main/java/net/diemond_player/unidye/UnidyeClient.java
@@ -0,0 +1,10 @@
+package net.diemond_player.unidye;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnidyeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
